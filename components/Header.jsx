159a6888e7643ddae41d590606ad7587fbabaac5
seybo <michaelatworkspace@gmail.com>
2020-05-03 21:59:25 +0530

    add polished and some header styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,32 @@
 import styled from "@emotion/styled";
+import { rem } from "polished";
 
-export default function Header() {
-  return <HeaderStyled>Header</HeaderStyled>;
+export default function Header({ isDark }) {
+  return (
+    <HeaderStyled isDark={isDark}>
+      <div className="container">
+        <div className="logo">
+          <img src="vercel.svg" alt="Sites logo" />
+          <span className="logo-text">NeXT movies</span>
+        </div>
+      </div>
+    </HeaderStyled>
+  );
 }
 
 const HeaderStyled = styled.header`
-  background: ${(props) => props.theme.colors.primary};
+  background: ${(props) => (props.isDark ? "#000000" : "#efefef")};
+  padding: 20px;
+
+  .logo {
+    display: flex;
+    align-items: center;
+  }
+
+  .logo-text {
+    color: #333333;
+    font-weight: bold;
+    font-size: ${rem(20)};
+    margin-left: 20px;
+  }
 `;
