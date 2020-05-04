c636b7ad243e3a5ddeb45b3e86bb2719c300a25e
seybo <michaelatworkspace@gmail.com>
2020-05-04 22:01:21 +0530

    list movies

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,24 @@
-import styled from "@emotion/styled";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ movies }) {
   return (
     <div className="container">
-      <SomeText>Front Page</SomeText>
+      {movies.map((movie) => {
+        return <div key={movie.id}>{movie.title}</div>;
+      })}
     </div>
   );
 }
 
-const SomeText = styled.div`
-  color: ${(props) => props.theme.colors.primary};
-`;
+export async function getStaticProps(_context) {
+  try {
+    const res = await axios.get(`${process.env.API_URL}/movies`);
+
+    return {
+      props: { movies: res.data }, // will be passed to the page component as props
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
